fb7a1245081f8765bfdbf9c1e90bde39843d6c6a
Nishanth_7 <nishanth298@gmail.com>
2019-09-07 01:17:45 +0530

    Converted to the MVC Design pattern

---
+++ ListMaker/ Data Model /item.swift
@@ -0,0 +1,17 @@
+//
+//  item.swift
+//  ListMaker
+//
+//  Created by Padmasri Nishanth on 9/6/19.
+//  Copyright © 2019 Padmasri Nishanth. All rights reserved.
+//
+
+import Foundation
+
+ class item
+{
+    //MARK :- Declaring Variables
+    
+    var title : String = ""
+    var done : Bool = false
+}
